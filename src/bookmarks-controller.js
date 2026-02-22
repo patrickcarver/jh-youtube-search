98fcbf75e7ce70a9aa02d9de91e1b32744d11c34
Patrick Carver <patrickcarver@gmail.com>
2026-02-22 17:42:01 -0600

    complete the bookmarks controller

--- src/bookmarks-controller.js
+++ src/bookmarks-controller.js
@@ -1,1 +1,44 @@
-import { BoomarksRepository } from "./bookmarks-repository.js";
+import { BookmarksRepository } from "./bookmarks-repository.js";
+
+export class BookmarksController {
+  #host = null;
+  #bookmarks = [];
+  #repository = null;
+
+  constructor(host) {
+    this.#host = host;
+    host.addController(this);
+  }
+
+  async hostConnected() {
+    this.#repository = new BookmarksRepository();
+    this.#bookmarks = await this.#repository.getAll();
+    this.#host.requestUpdate();
+  }
+
+  get bookmarks() {
+    return this.#bookmarks;
+  }
+
+  add(video) {
+    if (this.#bookmarks.some((b) => b.videoId === video.videoId)) return;
+    this.#bookmarks.push(video);
+    this.#repository.save(video);
+    this.#host.requestUpdate();
+  }
+
+  remove(videoId) {
+    this.#bookmarks.splice(
+      this.#bookmarks.findIndex((bookmark) => bookmark.videoId === videoId),
+      1,
+    );
+    this.#repository.delete(videoId);
+    this.#host.requestUpdate();
+  }
+
+  clearAll() {
+    this.#bookmarks = [];
+    this.#repository.clearAll();
+    this.#host.requestUpdate();
+  }
+}
