a1e02414575071265053515a9b4d23be05af073a
Patrick Carver <patrickcarver@gmail.com>
2026-02-22 12:38:45 -0600

    flesh out video grid

--- src/jh-video-grid.js
+++ src/jh-video-grid.js
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from "lit";
-import { JhVideoCard } from "./jh-video-card.js";
+import "./jh-video-card.js";
 
 export class JhVideoGrid extends LitElement {
   static properties = {
@@ -12,11 +12,39 @@ export class JhVideoGrid extends LitElement {
 
   constructor() {
     super();
+    this.videos = [];
+    this.allowSave = false;
+    this.allowDelete = false;
+    this.view = "grid";
+    this.loading = false;
   }
 
   render() {
+    if (this.loading) {
+      return html`
+        ${Array.from(
+          { length: 12 },
+          () => html`<div class="skeleton-card"></div>`,
+        )}
+      `;
+    }
+
+    if (this.videos.length === 0) {
+      return html` <div>No results</div>`;
+    }
+
     return html`
-      <div id="video-grid">${this.videos.map((video) => html``)}</div>
+      <div id="video-grid">
+        ${this.videos.map(
+          (video) => html`
+            <jh-video-card
+              .video=${video}
+              ?allowSave=${this.allowSave}
+              ?allowDelete=${this.allowDelete}
+            ></jh-video-card>
+          `,
+        )}
+      </div>
     `;
   }
 }
